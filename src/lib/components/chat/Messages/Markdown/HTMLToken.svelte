10498b9520a4c16ffadac1cf798a7620c75eda45
papajbeautiful <77177488+papajbeautiful@users.noreply.github.com>
2025-05-13 09:43:12 +1000

    Update HTMLToken.svelte
    allow same origin

--- src/lib/components/chat/Messages/Markdown/HTMLToken.svelte
+++ src/lib/components/chat/Messages/Markdown/HTMLToken.svelte
@@ -64,9 +64,7 @@
 				src={`${WEBUI_BASE_URL}/api/v1/files/${fileId}/content/html`}
 				title="Content"
 				frameborder="0"
-				sandbox="allow-scripts{($settings?.iframeSandboxAllowForms ?? false)
-					? ' allow-forms'
-					: ''}{($settings?.iframeSandboxAllowSameOrigin ?? false) ? ' allow-same-origin' : ''}"
+				sandbox="allow-scripts allow-same-origin allow-forms"
 				referrerpolicy="strict-origin-when-cross-origin"
 				allowfullscreen
 				width="100%"
